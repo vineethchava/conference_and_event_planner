a223b9f80edeba2cf2988b077eef3d581eb28262
VineethChava <vineethchava1997@gmail.com>
2024-08-26 18:38:06 -0500

    Added image urls

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,32 +4,32 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
         {
-        img: "",
+        img: "https://images.unsplash.com/photo-1528395874238-34ebe249b3f2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvamVjdG9yfGVufDB8fDB8fHww",
         name: "Projectors",
         cost: 200,
         quantity: 0,
     },
     {
-        img: "",
+        img: "https://images.unsplash.com/photo-1531104985437-603d6490e6d4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BlYWtlcnxlbnwwfHwwfHx8MA%3D%3D",
         name: "Speaker",
         cost: 35,
         quantity: 0,
     },
     {
-        img: "",
+        img: "https://images.unsplash.com/photo-1485579149621-3123dd979885?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWljcm9waG9uZXxlbnwwfHwwfHx8MA%3D%3D",
         name: "Microphones",
         cost: 45,
         quantity: 0,
     },
     {
-        img: "",
+        img: "https://plus.unsplash.com/premium_photo-1681671796265-4e8925f49df2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2hpdGVib2FyZHxlbnwwfHwwfHx8MA%3D%3D",
         name: "Whiteboards",
         cost: 80,
         quantity: 0,
     },
 
     {
-        img: "",
+        img: "https://images.unsplash.com/photo-1618255342875-a1d288c04939?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2lnbmFnZXxlbnwwfHwwfHx8MA%3D%3D",
         name: "Signage",
         cost: 80,
         quantity: 0,
--- src/venueSlice.js
+++ src/venueSlice.js
@@ -5,31 +5,31 @@ export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "",
+      img: "https://plus.unsplash.com/premium_photo-1661878787649-71d9b0713b8a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uZmVyZW5jZSUyMHJvb218ZW58MHx8MHx8fDA%3D",
       name: "Conference Room (Capacity:15)",
       cost: 3500,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://plus.unsplash.com/premium_photo-1680807869624-07b389d623e8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXVkaXRvcml1bSUyMHJvb218ZW58MHx8MHx8fDA%3D",
       name: "Auditorium Hall (Capacity:200)",
       cost: 5500,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://plus.unsplash.com/premium_photo-1705267936187-aceda1a6c1a6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHJlc2VudGF0aW9uJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D",
       name: "Presentation Room (Capacity:50)",
       cost: 700,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://plus.unsplash.com/premium_photo-1681487144031-d502ea9abefc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVldGluZyUyMHJvb218ZW58MHx8MHx8fDA%3D",
       name: "Large Meeting Room (Capacity:10)",
       cost: 900,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://plus.unsplash.com/premium_photo-1661495427374-2f73c71102e9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fG1lZXRpbmclMjByb29tfGVufDB8fDB8fHww",
       name: "Small Meeting Room (Capacity:5)",
       cost: 1100,
       quantity: 0,
