42d54c18ea6af5d66d53c8808e3c6b7283d1b017
VineethChava <vineethchava1997@gmail.com>
2024-08-26 18:27:56 -0500

    Updated Code

---
+++ src/venueSlice.js
@@ -0,0 +1,59 @@
+// venueSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+export const venueSlice = createSlice({
+  name: "venue",
+  initialState: [
+    {
+      img: "",
+      name: "Conference Room (Capacity:15)",
+      cost: 3500,
+      quantity: 0,
+    },
+    {
+      img: "",
+      name: "Auditorium Hall (Capacity:200)",
+      cost: 5500,
+      quantity: 0,
+    },
+    {
+      img: "",
+      name: "Presentation Room (Capacity:50)",
+      cost: 700,
+      quantity: 0,
+    },
+    {
+      img: "",
+      name: "Large Meeting Room (Capacity:10)",
+      cost: 900,
+      quantity: 0,
+    },
+    {
+      img: "",
+      name: "Small Meeting Room (Capacity:5)",
+      cost: 1100,
+      quantity: 0,
+    },
+  
+  ],
+  reducers: {
+   
+    incrementQuantity: (state, action) => {
+      const { payload: index } = action;
+      if (state[index]) {
+        if (state[index].name === " Auditorium Hall (Capacity:200)" && state[index].quantity >= 3) {
+          return;        }
+        state[index].quantity++;
+      }
+    },
+    decrementQuantity: (state, action) => {
+      const { payload: index } = action;
+      if (state[index] && state[index].quantity > 0) {
+        state[index].quantity--;
+      }
+    },
+  },
+});
+
+export const { incrementQuantity, decrementQuantity } = venueSlice.actions;
+export default venueSlice.reducer;
